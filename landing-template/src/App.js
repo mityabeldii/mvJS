29ee7acd1b5b81d8d7773e599c9ecad4a07fc1b5
mityabeldii <mityabeldii@gmail.com>
2019-04-21 11:54:08 +0300

    update landing-template

--- landing-template/src/App.js
+++ landing-template/src/App.js
@@ -1,87 +1,53 @@
 /*eslint-disable no-unused-vars*/
 import React, { useState } from 'react'
 import styled from 'styled-components'
+import Scroller from './Scroller'
 
-let screens = [
-    {
-        name: `first`,
-        color: `blue`,
-    },
-    {
-        name: `second`,
-        color: `green`,
-    }
-]
+let array = [0, 1, 2]
 
 let App = () => {
-    let [currentScreen, setCurrentScreen] = useState(0)
-    return (
-        <Wrapper>
-            <Header>
-                <MenuItem>{currentScreen}</MenuItem>
-                {
-                    screens.map((i, index) => {
-                        return (
-                            <MenuItem
-                                key={index}
-                                color={i.color}
-                                onClick={() => { setCurrentScreen(index) }}
-                            >
-                                {i.name}
-                            </MenuItem>
-                        )
-                    })
-                }
-            </Header>
-            <Workspace>
-                {
-                    screens.map((i, index) => {
-                        return (
-                            <Block key={index} color={i.color}>
-
-                            </Block>
-                        )
-                    })
-                }
-            </Workspace>
-        </Wrapper>
-    )
+    let [screenNumber, setScreenNumber] = useState(0)
+    return <Wrapper>
+        <Header>
+            {
+                array.map((item, index) => {
+                    return (
+                        <div key={index} onClick={() => { setScreenNumber(index) }} >screen {index}</div>
+                    )
+                })
+            }
+        </Header>
+        <Scroller screenNumber={screenNumber} >
+            {
+                array.map((item, index) => {
+                    return (
+                        <Block key={index}>{index}</Block>
+                    )
+                })
+            }
+        </Scroller>
+    </Wrapper>
 }
 
 export default App
 
 const Wrapper = styled.div`
-display: flex
-justify-content: center
-align-items: center
-flex-direction: column
-transition: 0.2s;
 width: 100vw;
 height: 100vh;
-background-color: white;
 @media (min-width: 320px) and (max-width: 480px) {
     
 }`
 
 const Header = styled.div`
 display: flex
-justify-content: center
+justify-content: space-around
 align-items: center
-flex-direction: row;
+flex-direction: row
 transition: 0.2s
 width: 100vw;
 height: 8vh;
-background-color: red;
-@media (min-width: 320px) and (max-width: 480px) {
-    
-}`
-
-const Workspace = styled.div`
-display: block;
-overflow: scroll;
-width: 100vw;
-height: 92vh;
-background-color: yellow;
+position: fixed;
+top: 0;
 @media (min-width: 320px) and (max-width: 480px) {
     
 }`
@@ -93,22 +59,7 @@ align-items: center
 flex-direction: column
 transition: 0.2s
 width: 100vw;
-height: 92vh;
-background-color: ${props => props.color}
-@media (min-width: 320px) and (max-width: 480px) {
-    
-}`
-
-const MenuItem = styled.div`
-display: flex
-justify-content: center
-align-items: center
-flex-direction: column
-transition: 0.2s
-width: 10vw;
-height: 8vh;
-background-color: white;
-cursor: pointer;
+height: 100vh;
 @media (min-width: 320px) and (max-width: 480px) {
     
 }`
