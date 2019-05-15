2f00922749f87b13040c023fc55b9db263ef4981
PeterBelzman <petka751@gmail.com>
2019-05-16 00:11:45 +0300

    add  two blocks on secon page and tree div-s inside downer block

--- landing-template/src/first.js
+++ landing-template/src/first.js
@@ -1,19 +1,43 @@
 import React from 'react'
 import styled from "styled-components";
 
-const ParagInside = () => <p>wefwefwefwef</p>
+const TryToday = () => <p>Попробуйте сегодня</p>;
 
-const RectangleBlockFirst = () => <RectangleBlock bgColorStyle = {`purple`}>
-<ParagInside/>
+const RectangleBlockFirst = () => <RectangleBlock bgColorStyle = {`purple`} />;
+const RectangleBlockSecond = () => < RectangleBlock bgColorStyle = {`green`}>
+    <WrapLevelOne heightStyle = {`70%`} borderColor = {`yellow`}/>
+    <WrapLevelOne heightStyle = {`30%`} borderColor = {`red`}>
+        <WrapLevelTwo widthStyle = {`50%`}>
+        <TryToday/>
+        </WrapLevelTwo>
+        <WrapLevelTwo widthStyle = {`30%`}/>
+        <WrapLevelTwo widthStyle = {`20%`}/>
+    </WrapLevelOne>
 </RectangleBlock>
-const RectangleBlockSecond = () => < RectangleBlock bgColorStyle = {`green`}/>
 const RectangleBlockThird = () => < RectangleBlock bgColorStyle = {`blue`}/>
 
 
 const RectangleBlock = styled.section `
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     width: 100vw;
     height: 100vh;
     background-color: ${props => props.bgColorStyle}
+`;
+
+const WrapLevelOne = styled.section `
+    display: flex;
+    width: 95%;
+    height: ${props => props.heightStyle};
+    border: 1px solid ${props => props.borderColor};;
+`;
+
+const WrapLevelTwo = styled.div `
+    display: flex;
+    width: ${props => props.widthStyle};
+    height: 100%;
+    border: 1px solid blue;
 `;
 
 export default [RectangleBlockFirst, RectangleBlockSecond, RectangleBlockThird];
