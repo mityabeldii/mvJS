7c7bc71d09917224a4027fca2577000b215d7fdd
PeterBelzman <petka751@gmail.com>
2019-05-22 21:52:59 +0300

    MVJS-6: add entry wrap

---
+++ landing-template/src/styled-components/PageEntryStyle.js
@@ -0,0 +1,21 @@
+/*eslint-disable no-unused-vars*/
+import styled from "styled-components";
+// import * as Variables from "./styled-components/VariableStyle";
+
+export const EntryWrap = styled.div`
+    display: flex;
+    width: 95%;
+    height: 100%;
+    @media (min-width: 320px) and (max-width: 768px) {
+            
+        }`;
+
+export const EntryBlock = styled.div`
+    display: flex;
+    width: 50%;
+    height: 100%;
+    border: 1px solid red;
+    @media (min-width: 320px) and (max-width: 768px) {
+            
+        }`;
+/*eslint-enable no-unused-vars*/
---
+++ landing-template/src/styled-components/PageVideoStyle.js
@@ -0,0 +1,23 @@
+/*eslint-disable no-unused-vars*/
+import styled from "styled-components";
+// import * as Variables from "./styled-components/VariableStyle";
+
+// style of third page
+export const WrapVideo = styled.section `
+    display: flex;
+    width: 100%;
+    height: 80%;
+    border: 1px solid red;
+    @media (min-width: 320px) and (max-width: 768px) {
+            
+        }`;
+
+export const WrapTourWatch = styled.div `
+    display: flex;
+    width: 100%;
+    height: 20%;
+    border: 1px solid yellow;
+    @media (min-width: 320px) and (max-width: 768px) {
+            
+        }`;
+/*eslint-enable no-unused-vars*/
