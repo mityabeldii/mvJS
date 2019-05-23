2a106816b12025a84c69ad4f505c3a449be3cf6d
PeterBelzman <petka751@gmail.com>
2019-05-24 01:03:43 +0300

    MVJS-8: add schedule table structure

--- landing-template/src/screens.js
+++ landing-template/src/screens.js
@@ -68,20 +68,39 @@ const PageVideo = (props) => <LayoutStyle.RectangleBlock bgColorStyle={`rgba(0,2
     <PageVideoStyle.WrapTourWatch />
 </LayoutStyle.RectangleBlock>
 
-// 6 page - Schedule
-const week = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
-// let weekDay = week.map((i) => 
-//     return { <PageScheduleStyle.ScheduleTableHeadItem><p>day</p></PageScheduleStyle.ScheduleTableHeadItem>}
-// );
+// 4 page
 
+// 6 page - Schedule
+const week = ['', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+const dayTime = ['9-00', '10-00', '11-00', '12-00', '13-00', '14-00', '15-00', '16-00', '17-00', '18-00', '19-00', '20-00', '21-00'];
+/* Элемент шапки таблицы */ 
+const weekDay = week.map((elem, index) => {
+    return <PageScheduleStyle.ScheduleTableHeadItem key={index}><p>{elem}</p></PageScheduleStyle.ScheduleTableHeadItem>
+});
+/* Формирование таблицы */ 
+const tableWeek = dayTime.map((row, row_index) => {
+     /* Ряд таблицы */
+    return <PageScheduleStyle.ScheduleTableRow key={row_index}>
+                { 
+                    /* Элемент таблицы */ 
+                    week.map((column, column_index) => {
+                            return <PageScheduleStyle.ScheduleTableItem key={column_index}>
+                                        <p>{ column_index === 0 ? row : `some data` }</p>
+                                </PageScheduleStyle.ScheduleTableItem>
+                            }) 
+                }
+    </PageScheduleStyle.ScheduleTableRow>
+});
+
+const day = 'суббота';
 const PageSchedule = (props) => <LayoutStyle.RectangleBlock bgColorStyle={`rgba(0,0,255,0.5)`}>
     <PageScheduleStyle.WrapScheduleTable>
-        <p>расписание</p>
+        <p>{`расписание на ${day}`}</p>
         <PageScheduleStyle.ScheduleTableHead>
-                    
+            {weekDay}     
         </PageScheduleStyle.ScheduleTableHead>
         <PageScheduleStyle.ScheduleTable>
-            
+            {tableWeek}
         </PageScheduleStyle.ScheduleTable>
     </PageScheduleStyle.WrapScheduleTable>
 </LayoutStyle.RectangleBlock>
--- landing-template/src/styled-components/VariableStyle.js
+++ landing-template/src/styled-components/VariableStyle.js
@@ -1,4 +1,5 @@
 /*eslint-disable no-unused-vars*/
 // colors
 export const _$white = `#ffffff`;
+export const _$black = `#000000`;
 /*eslint-enable no-unused-vars*/
